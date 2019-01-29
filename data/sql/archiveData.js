657a17ac9e19d3df4dc443a75e357c1c958d199f
deeper-x <albertodeprezzo@gmail.com>
2019-01-29 17:15:32 +0100

    issue #14

--- data/sql/archiveData.js
+++ data/sql/archiveData.js
@@ -4,12 +4,21 @@ let tripsArchive = function (idPortinformer) {
             control_unit_data.is_active AS in_progress,
             shipping_details.ts_arrival AS arrival,
             shipping_details.ts_departure AS departure,
-            ship_description AS ship
+            ship_description AS ship,
+            shipped_goods_details
             FROM control_unit_data INNER JOIN ships
             ON control_unit_data.fk_ship = ships.id_ship
             INNER JOIN shipping_details
             ON control_unit_data.fk_shipping_details = id_shipping_details
-            WHERE control_unit_data.fk_portinformer = ${idPortinformer} `;
+            INNER JOIN ( 
+                SELECT fk_control_unit_data, string_agg(shipped_goods.goods_mvmnt_type||' '||goods_categories.description||' '||shipped_goods.quantity||' '||shipped_goods.unit||',', '-')
+                AS shipped_goods_details
+                FROM shipped_goods INNER JOIN goods_categories
+                ON fk_goods_category = id_goods_category
+                GROUP BY fk_control_unit_data
+            ) AS shipped_goods_data
+            ON shipped_goods_data.fk_control_unit_data = id_control_unit_data
+            WHERE control_unit_data.fk_portinformer = ${idPortinformer}`;
 };
 
 let archiveData = {
