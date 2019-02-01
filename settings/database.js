95c124d94fcd54a2bc1cdf2aeb288d1e89164adf
deeper-x <albertodeprezzo@gmail.com>
2019-02-01 14:05:02 +0100

    Create database.js

---
+++ settings/database.js
@@ -0,0 +1,9 @@
+const databaseConfig = {
+    user: <INSERT USER>',
+    host: '<INSERT HOST>',
+    password: '<INSERT PASSWORD>',
+    database: '<INSERT DB>',
+    port: 5432
+};
+
+module.exports = databaseConfig;
