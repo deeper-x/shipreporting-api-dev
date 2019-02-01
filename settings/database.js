d677231c4e82ccafee0926f467baa6ff08e7ed1b
deeper-x <albertodeprezzo@gmail.com>
2019-02-01 13:43:13 +0100

    updated settings

--- settings/database.js
+++ settings/database.js
@@ -1,7 +1,10 @@
 const databaseConfig = {
-    user: '********',
+    user: 'lims_cn_user',
     host: '127.0.0.1',
-    password: '*******',
-    database: '*******',
+    password: '07R3BL4',
+    database: 'lims_cn_sw',
     port: 5432
 };
+
+
+module.exports = databaseConfig;
