940fe7ed8424b04a8cb3b248651ecf4bd735708c
deeper-x <albertodeprezzo@gmail.com>
2019-02-04 12:03:50 +0100

    updated gitignore

--- settings/configuration.js
+++ settings/configuration.js
@@ -34,3 +34,4 @@ class Configuration {
 
 
 module.exports = Configuration;
+
--- settings/db_auth.js
+++
@@ -1,9 +0,0 @@
-const databaseConfig = {
-    user: 'XXXXXXXXXX',
-    host: '127.0.0.1',
-    password: '********',
-    database: 'XXXXXXXX',
-    port: 5432
-};
-
-module.exports = databaseConfig;
