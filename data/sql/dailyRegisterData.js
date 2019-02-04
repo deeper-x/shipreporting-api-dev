ac8d757040331e77e57389bd5183cd462591d9c1
deeper-x <albertodeprezzo@gmail.com>
2019-02-04 18:27:29 +0100

    issue #4

--- data/sql/dailyRegisterData.js
+++ data/sql/dailyRegisterData.js
@@ -82,11 +82,38 @@ let registerDepartures = function (idPortinformer) {
             AND ts_out_of_sight BETWEEN (select current_date - 1||' (SELECT day_start_time FROM portinformers WHERE id_portinformer = ${idPortinformer})') AND (select current_date||' (SELECT day_start_time FROM portinformers WHERE id_portinformer = ${idPortinformer})')`;
 };
 
+let registerShiftings = function (idPortinformer, shiftingStates) {
+    return `SELECT ts_main_event_field_val, imo, ship_description, type_acronym,
+            iso3 
+            FROM control_unit_data 
+            INNER JOIN trips_logs
+            ON id_control_unit_data = trips_logs.fk_control_unit_data
+            INNER JOIN ships
+            ON control_unit_data.fk_ship = id_ship
+            INNER JOIN ship_types
+            ON id_ship_type = fk_ship_type
+            INNER JOIN countries
+            ON ships.fk_country_flag = id_country
+            WHERE control_unit_data.fk_portinformer = ${idPortinformer}
+            AND fk_state IN ${shiftingStates}
+            AND ts_main_event_field_val 
+            BETWEEN 
+            (select current_date - 1||' 
+                                        (SELECT day_start_time FROM portinformers WHERE id_portinformer = ${idPortinformer})
+                                    ') 
+            AND 
+            (select current_date||' 
+                                (SELECT day_start_time FROM portinformers WHERE id_portinformer = ${idPortinformer}
+                                ')
+            ORDER BY ts_main_event_field_val DESC LIMIT 1`;
+};
+
 let dailyRegisterData = {
     registerArrivals: registerArrivals,
     registerMoored: registerMoored,
     registerRoadstead: registerRoadstead,
     registerDepartures: registerDepartures,
+    registerShiftings: registerShiftings,
 };
 
 module.exports = dailyRegisterData;
