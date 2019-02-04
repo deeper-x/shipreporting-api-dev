e9541651c7e73f5e93215ff48c50fc08eb31353f
deeper-x <albertodeprezzo@gmail.com>
2019-02-04 11:03:26 +0100

    Create db_auth.js

---
+++ settings/db_auth.js
@@ -0,0 +1,10 @@
+const databaseConfig = {
+    user: '<USER>',
+    host: '<HOST',
+    password: '<PASSWORD>',
+    database: '<DBNAME>',
+    port: <PORT>
+};
+
+module.exports = databaseConfig;
+~                                     
