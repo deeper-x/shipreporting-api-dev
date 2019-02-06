6f7ab13dd1a9285e228489858c83a05eb4d1e8ff
deeper-x <albertodeprezzo@gmail.com>
2019-02-06 10:54:58 +0100

    issue #31

--- data/sql/archiveData.js
+++ data/sql/archiveData.js
@@ -5,23 +5,52 @@ let tripsArchive = function (idPortinformer) {
             shipping_details.ts_arrival AS arrival,
             shipping_details.ts_departure AS departure,
             ship_description AS ship,
-            shipped_goods_details
+            shipped_goods_details_loaded,
+            shipped_goods_details_unloaded,
+            shipped_goods_details_transit
             FROM control_unit_data INNER JOIN ships
             ON control_unit_data.fk_ship = ships.id_ship
             INNER JOIN shipping_details
             ON control_unit_data.fk_shipping_details = id_shipping_details
             INNER JOIN ( 
                 SELECT fk_control_unit_data, string_agg(shipped_goods.goods_mvmnt_type||':'||goods_categories.description||' '||shipped_goods.quantity||''||shipped_goods.unit||' ['||quays.description||'-'||berths.description||']' , '<br />')
-                AS shipped_goods_details
+                AS shipped_goods_details_loaded
                 FROM shipped_goods INNER JOIN goods_categories
                 ON fk_goods_category = id_goods_category
                 INNER JOIN quays
                 ON shipped_goods.fk_operation_quay = id_quay
                 INNER JOIN berths
                 ON shipped_goods.fk_operation_berth = id_berth
+                WHERE shipped_goods.goods_mvmnt_type = 'LO'
                 GROUP BY fk_control_unit_data
-            ) AS shipped_goods_data            
-            ON shipped_goods_data.fk_control_unit_data = id_control_unit_data
+            ) AS shipped_goods_data_loaded            
+            ON shipped_goods_data_loaded.fk_control_unit_data = id_control_unit_data
+            INNER JOIN ( 
+                SELECT fk_control_unit_data, string_agg(shipped_goods.goods_mvmnt_type||':'||goods_categories.description||' '||shipped_goods.quantity||''||shipped_goods.unit||' ['||quays.description||'-'||berths.description||']' , '<br />')
+                AS shipped_goods_details_unloaded
+                FROM shipped_goods INNER JOIN goods_categories
+                ON fk_goods_category = id_goods_category
+                INNER JOIN quays
+                ON shipped_goods.fk_operation_quay = id_quay
+                INNER JOIN berths
+                ON shipped_goods.fk_operation_berth = id_berth
+                WHERE shipped_goods.goods_mvmnt_type = 'UN'
+                GROUP BY fk_control_unit_data
+            ) AS shipped_goods_data_unloaded            
+            ON shipped_goods_data_unloaded.fk_control_unit_data = id_control_unit_data
+            INNER JOIN ( 
+                SELECT fk_control_unit_data, string_agg(shipped_goods.goods_mvmnt_type||':'||goods_categories.description||' '||shipped_goods.quantity||''||shipped_goods.unit||' ['||quays.description||'-'||berths.description||']' , '<br />')
+                AS shipped_goods_details_transit
+                FROM shipped_goods INNER JOIN goods_categories
+                ON fk_goods_category = id_goods_category
+                INNER JOIN quays
+                ON shipped_goods.fk_operation_quay = id_quay
+                INNER JOIN berths
+                ON shipped_goods.fk_operation_berth = id_berth
+                WHERE shipped_goods.goods_mvmnt_type = 'TR'
+                GROUP BY fk_control_unit_data
+            ) AS shipped_goods_data_transit            
+            ON shipped_goods_data_transit.fk_control_unit_data = id_control_unit_data
             WHERE control_unit_data.fk_portinformer = ${idPortinformer}`;
 };
 
