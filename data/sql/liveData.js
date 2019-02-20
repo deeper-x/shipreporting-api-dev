7beb8753da54777059021e4b238c0e38f286b0f1
deeper-x <albertodeprezzo@gmail.com>
2019-02-20 19:02:55 +0100

    issue #95

--- data/sql/liveData.js
+++ data/sql/liveData.js
@@ -24,7 +24,7 @@ let moored = function (idPortinformer, idCurrentActivity, notOperationalStates) 
         ON control_unit_data.id_control_unit_data = TL.fk_control_unit_data
         INNER JOIN shipping_details
         ON shipping_details.id_shipping_details = control_unit_data.fk_shipping_details
-        LEFT JOIN (
+        INNER JOIN (
             SELECT fk_control_unit_data, string_agg(goods_mvmnt_type||'->'||goods_categories.description::TEXT||'-'||groups_categories.description, ', ') AS shipped_goods_row
             FROM shipped_goods
             INNER JOIN goods_categories
@@ -34,14 +34,6 @@ let moored = function (idPortinformer, idCurrentActivity, notOperationalStates) 
             GROUP BY fk_control_unit_data        
         ) as shipped_goods_data
         ON shipped_goods_data.fk_control_unit_data = control_unit_data.id_control_unit_data
-        LEFT JOIN (
-                SELECT fk_control_unit_data, string_agg(traffic_list_mvnt_type, '-')
-                FROM traffic_list
-                INNER JOIN traffic_list_categories
-                ON traffic_list_categories.id_traffic_list_category = traffic_list.fk_traffic_list_category
-                GROUP BY fk_control_unit_data
-        ) AS traffic_list_data
-        ON control_unit_data.id_control_unit_data = traffic_list_data.fk_control_unit_data
         INNER JOIN ports
         ON shipping_details.fk_port_provenance = ports.id_port 
         INNER JOIN ships
@@ -94,13 +86,6 @@ let roadstead = function (idPortinformer, idCurrentActivity, notOperationalState
             GROUP BY fk_control_unit_data        
         ) as shipped_goods_data
         ON shipped_goods_data.fk_control_unit_data = control_unit_data.id_control_unit_data
-        INNER JOIN (
-            SELECT fk_control_unit_data, string_agg(traffic_list_mvnt_type, '-')
-            FROM traffic_list
-            INNER JOIN traffic_list_categories
-            ON id_traffic_list_category = fk_traffic_list_category
-        ) AS traffic_list_data
-        ON control_unit_data.id_control_unit_data = traffic_list_category.fk_control_unit_data
         INNER JOIN ports
         ON shipping_details.fk_port_provenance = ports.id_port 
         INNER JOIN ships
