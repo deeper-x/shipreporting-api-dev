4249cf7d1fc0cf41e97e257b997e2e2f991997ec
deeper-x <albertodeprezzo@gmail.com>
2019-05-13 10:31:30 +0200

    update A3

--- data/sql/liveData.js
+++ data/sql/liveData.js
@@ -1,65 +1,46 @@
-let moored = function (idPortinformer, idCurrentActivity, notOperationalStates) {
-    return `SELECT RES.fk_control_unit_data as id_trip, ship_description as ship, 
-        ts_main_event_field_val, quays.description as quay, berths.description as berth, 
-        type_acronym as ship_type, iso3, gross_tonnage, ships.length, ships.width,
-        ports.name as port, agencies.description as agency, 
+let moored = function (idPortinformer) {
+    return `SELECT id_control_unit_data, ship_description, 
+        ts_last_ship_activity, 
+        ship_current_activities.description AS current_activity, 
+        quays.description AS quay,
         shipped_goods_data.shipped_goods_row AS shipped_goods_data,
-        traffic_list_data.traffic_list_details AS traffic_list_details
-        FROM (  
-            SELECT fk_control_unit_data, MAX(ts_main_event_field_val) AS max_time, fk_portinformer
-            FROM trips_logs
-            WHERE fk_state NOT IN ${notOperationalStates}
-            GROUP BY fk_control_unit_data, fk_portinformer
-            ) 
-        AS RES
-        INNER JOIN trips_logs AS TL 
-        ON RES.fk_control_unit_data = TL.fk_control_unit_data
-        INNER JOIN agencies
-        ON TL.fk_agency = agencies.id_agency
-        INNER JOIN maneuverings
-        ON TL.fk_maneuvering = maneuverings.id_maneuvering
+        iso3, gross_tonnage, ships.length, ships.width, type_acronym,
+        agencies.description AS agency  
+        FROM control_unit_data 
+        INNER JOIN ships
+        ON fk_ship = id_ship
+        INNER JOIN ship_current_activities
+        ON fk_ship_current_activity = id_activity
+        INNER JOIN latest_maneuverings
+        ON latest_maneuverings.fk_control_unit_data = id_control_unit_data
         INNER JOIN quays
-        ON maneuverings.fk_stop_quay = quays.id_quay
-        INNER JOIN berths
-        ON maneuverings.fk_stop_berth = berths.id_berth
-        INNER JOIN control_unit_data
-        ON control_unit_data.id_control_unit_data = TL.fk_control_unit_data
-        INNER JOIN shipping_details
-        ON shipping_details.id_shipping_details = control_unit_data.fk_shipping_details
+        ON latest_maneuverings.fk_stop_quay = id_quay
         LEFT JOIN (
-            SELECT fk_control_unit_data, string_agg(goods_mvmnt_type||'->'||goods_categories.description::TEXT||'-'||groups_categories.description, ', ') AS shipped_goods_row
-            FROM shipped_goods
-            INNER JOIN goods_categories
-            ON goods_categories.id_goods_category = shipped_goods.fk_goods_category
-            INNER JOIN groups_categories
-            ON groups_categories.id_group = goods_categories.fk_group_category
-            GROUP BY fk_control_unit_data        
+                SELECT fk_control_unit_data, string_agg(goods_mvmnt_type||':'||goods_categories.description::TEXT||'-'||groups_categories.description, ', ') AS shipped_goods_row
+                FROM shipped_goods
+                INNER JOIN goods_categories
+                ON goods_categories.id_goods_category = shipped_goods.fk_goods_category
+                INNER JOIN groups_categories
+                ON groups_categories.id_group = goods_categories.fk_group_category
+                GROUP BY fk_control_unit_data        
         ) as shipped_goods_data
         ON shipped_goods_data.fk_control_unit_data = control_unit_data.id_control_unit_data
-        LEFT JOIN (
-                SELECT fk_control_unit_data, string_agg(traffic_list_mvnt_type||'-'||description, '-') AS traffic_list_details
-                FROM traffic_list
-                INNER JOIN traffic_list_categories
-                ON traffic_list_categories.id_traffic_list_category = traffic_list.fk_traffic_list_category
-                GROUP BY fk_control_unit_data
-        ) AS traffic_list_data
-        ON control_unit_data.id_control_unit_data = traffic_list_data.fk_control_unit_data
-        INNER JOIN ports
-        ON shipping_details.fk_port_provenance = ports.id_port 
-        INNER JOIN ships
-        ON control_unit_data.fk_ship = id_ship
+        INNER JOIN (  
+                SELECT fk_control_unit_data, MAX(ts_main_event_field_val) AS max_time, fk_agency
+                FROM trips_logs
+                WHERE fk_portinformer = ${idPortinformer}
+                GROUP BY fk_control_unit_data, fk_portinformer, fk_agency
+                ) AS RES
+        ON id_control_unit_data = RES.fk_control_unit_data 
+        INNER JOIN countries
+        ON countries.id_country = ships.fk_country_flag
         INNER JOIN ship_types
         ON ships.fk_ship_type = ship_types.id_ship_type
-        INNER JOIN countries
-        ON countries.id_country = ships.fk_country_flag 
-        AND ts_main_event_field_val = max_time
-        WHERE control_unit_data.fk_portinformer = ${idPortinformer}
-        AND fk_ship_current_activity = ${idCurrentActivity}
-        AND is_active = true
-        GROUP BY id_trip, ts_main_event_field_val, ship, 
-        quay, berth, ship_type, iso3, gross_tonnage, ships.length, 
-        ships.width, port, agency, shipped_goods_row, traffic_list_details
-        ORDER BY RES.fk_control_unit_data`;
+        INNER JOIN agencies
+        ON RES.fk_agency = agencies.id_agency 
+        WHERE fk_ship_current_activity = 5
+        AND control_unit_data.is_active = true 
+        AND control_unit_data.fk_portinformer = ${idPortinformer}`;
 };  
 
 let roadstead = function (idPortinformer) {
